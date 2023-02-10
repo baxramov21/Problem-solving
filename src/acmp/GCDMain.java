f479c76a2a1f39734c4eb0bd72144209563af160
Sheikh <barsamaniya11@gmail.com>
2023-02-10 11:01:49 +0500

    GCD and GCM

--- src/acmp/GCDMain.java
+++ src/acmp/GCDMain.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class GCDMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(GCM.gcm(scanner.nextInt(), scanner.nextInt()));
+        System.out.println(Oranges.divideOranges(scanner.nextInt(), scanner.nextInt()));
     }
 
 }
@@ -29,5 +29,22 @@ class GCD {
 class GCM {
     static int gcm(int a, int b) {
         return (a * b) / GCD.gcd(a, b);
+    }
+}
+
+    /*
+   Катя решила пригласить к себе в гости n друзей. Так как ее друзья очень любят фрукты, то в качестве угощения для них
+   она купила m одинаковых апельсинов.
+
+Она хочет разрезать каждый апельсин на одинаковое число равных долек так, чтобы их можно было распределить
+между гостями (сама Катя апельсины есть не будет), и всем гостям досталось поровну долек.
+
+Напишите программу, которая вычисляет минимальное количество долек, на которое необходимо разрезать каждый апельсин,
+ чтобы были выполнены указанные выше условия.
+     */
+
+class Oranges {
+    static int divideOranges(int guestAmount, int orangeAmount) {
+        return guestAmount / GCD.gcd(guestAmount, orangeAmount);
     }
 }

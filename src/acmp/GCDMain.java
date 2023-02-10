af57f1c23d68218f316a2fb8c69ebfcf2d84f2f0
Sheikh <barsamaniya11@gmail.com>
2023-02-10 10:32:14 +0500

    GCD and GCM

--- src/acmp/GCDMain.java
+++ src/acmp/GCDMain.java
@@ -5,17 +5,29 @@ import java.util.Scanner;
 public class GCDMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(gcm(scanner.nextInt(), scanner.nextInt()));
+        System.out.println(GCM.gcm(scanner.nextInt(), scanner.nextInt()));
     }
 
+}
+
+/*
+Даны два натуральных числа A и B. Требуется найти их наибольший общий делитель (НОД).
+ */
+
+class GCD {
     static int gcd(int a, int b) {
         if (b == 0) {
             return a;
         }
         return gcd(b, a % b);
     }
+}
 
+/*
+Требуется написать программу, определяющую наименьшее общее кратное (НОК) чисел a и b.
+ */
+class GCM {
     static int gcm(int a, int b) {
-        return (a * b) / gcd(a, b);
+        return (a * b) / GCD.gcd(a, b);
     }
 }
